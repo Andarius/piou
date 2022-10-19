b49e184f62285beeb8f2dbf757e0bb3b8fae0537
Julien Brayere <julien.brayere@obitrain.com>
2022-10-19 14:52:11 +0200

    fix: bump version

--- .circleci/scripts/release.sh
+++ .circleci/scripts/release.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 cd "$(dirname "$0")/../.." || exit
 
 git config --global user.email "$CI_EMAIL"
 git config --global user.name "$CI_USER"
 
-readonly RETURN_CODE=$(poetry run cz bump --changelog --yes)
+poetry run cz bump --changelog --yes
 
-if [ "$RETURN_CODE" -eq 0 ]; then
+if [ $? -eq 0 ]; then
   git push origin master
   readonly TAG=$(poetry version -s)
   poetry build
