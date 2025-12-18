e40c4432b569ee460a65d1776ea6d0c6c3fb86a1
julien <julien.brayere@obitrain.com>
2025-12-18 20:03:11 +0100

    feat: adding support for annotated syntax

--- piou/__init__.py
+++ piou/__init__.py
@@ -1,9 +1,12 @@
+from typing import Annotated
+
 from .cli import Cli, CommandGroup
 from .utils import Option, Derived, Password
 from .command import CommandMeta
 from .exceptions import CommandNotFoundError, CommandError
 
 __all__ = (
+    "Annotated",
     "Cli",
     "CommandGroup",
     "Option",
