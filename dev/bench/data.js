9c599fddebd48c81d6995d6e5cebbaf17b52e563
github-action-benchmark <github@users.noreply.github.com>
2026-01-30 21:24:56 +0000

    add Import Performance (customSmallerIsBetter) benchmark result for dc473dd6b3dbbec6cac8cf1d09174eb39cb07008

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1769808296581,
+  "repoUrl": "https://github.com/Andarius/piou",
+  "entries": {
+    "Import Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "julien.brayere@obitrain.com",
+            "name": "Julien Brayere",
+            "username": "Andarius"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dc473dd6b3dbbec6cac8cf1d09174eb39cb07008",
+          "message": "fix: clean deps (#34)",
+          "timestamp": "2026-01-30T22:24:25+01:00",
+          "tree_id": "c331825e01da8f20688d441e74b373716ab918c6",
+          "url": "https://github.com/Andarius/piou/commit/dc473dd6b3dbbec6cac8cf1d09174eb39cb07008"
+        },
+        "date": 1769808296150,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "piou (core)",
+            "value": 93.92,
+            "unit": "ms",
+            "range": 1.14
+          },
+          {
+            "name": "piou (rich)",
+            "value": 168.77,
+            "unit": "ms",
+            "range": 1.3
+          },
+          {
+            "name": "piou.tui",
+            "value": 284.82,
+            "unit": "ms",
+            "range": 2.29
+          }
+        ]
+      }
+    ]
+  }
+}
